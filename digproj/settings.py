cfff90016656abb0d175e9bbd64a64c5f1aae2dc
mans-18 <miguel.sza@gmail.com>
2021-07-12 03:03:34 -0300

    dig back deploy prepared and python 3.7.11

--- digproj/settings.py
+++ digproj/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'digproj.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl)}
+# DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl)}
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
