10e9d15579022b8bbbc5a6d622b7b8fffe000e83
mans-18 <miguel.sza@gmail.com>
2022-11-13 14:43:46 -0300

    secure keys and email ok

--- digproj/settings.py
+++ digproj/settings.py
@@ -25,10 +25,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost:4200',
     'digestback.herokuapp.com',
-    'https://digestback.herokuapp.com/api/persona/email/',
+    #'https://digestback.herokuapp.com/api/persona/email/',
     #'digest.com.br',
     #'https://digest.com.br',
-    #'https://www.digest.com.br',
+    'https://www.digest.com.br',
 ]
 
 DEBUG = config('DEBUG', default=False, cast=bool)
