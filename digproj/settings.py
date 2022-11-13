4538202fb80a632597d11763f755ef9fa022b268
mans-18 <miguel.sza@gmail.com>
2022-11-13 15:33:28 -0300

    secure keys and email ok

--- digproj/settings.py
+++ digproj/settings.py
@@ -127,6 +127,8 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'bdqmangfsxogsgbc' #os.environ.get('EMAIL_HOST_PASSWORD_DIGPROJ')
+
 EMAIL_HOST_USER = 'miguel.sza@gmail.com'
 EMAIL_PORT = 587
 # EMAIL_PORT = 465
